8efc358499bdeb4408ca2255ebfa9d4fe66b389c
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2024-08-11 05:51:38 -0700

    remove comma

--- src/compnents/About.jsx
+++ src/compnents/About.jsx
@@ -21,7 +21,7 @@ function About() {
             I&apos;m a web and application developer with a passion for creating beautiful and functional digital solutions. My journey began with my studies in computer science, which I completed at SMIT. During my time there, I developed a strong foundation in programming and web development, which has been crucial in my career.
           </span>
           <span className='text-light md:text-lg'>
-            I specialize in using modern technologies like React and Next.js. With React, I build dynamic and responsive user interfaces that provide an excellent user experience. Next.js allows me to create powerful server-rendered applications that are fast and SEO-friendly. I'm also proficient with various libraries and tools that complement these technologies, ensuring I can tackle a wide range of projects efficiently.
+            I specialize in using modern technologies like React and Next.js. With React, I build dynamic and responsive user interfaces that provide an excellent user experience. Next.js allows me to create powerful server-rendered applications that are fast and SEO-friendly. I am also proficient with various libraries and tools that complement these technologies, ensuring I can tackle a wide range of projects efficiently.
           </span>
         </motion.div>
         <motion.div
