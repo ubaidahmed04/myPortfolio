d91f303e94bbbfe5bd26a15ba2879cdbdb466f38
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2024-08-11 12:01:28 -0700

    add tool card

--- README.md
+++ README.md
@@ -1,36 +1,44 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+## 🚀 Getting Started
 
-## Getting Started
+### Prerequisites
+Make sure you have Node.js and npm installed on your machine.
 
-First, run the development server:
+### How to Clone the Project
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+1. **Clone the repository:**
+   ```bash
+     https://github.com/SharjeelHussain877/certificate-generation-portal.git
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+### *Navigate to the project directory:
+  cd ./dir
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+### *Install dependencies
+    npm install
 
-## Learn More
+### *Run the development server:
+    npm run dev
 
-To learn more about Next.js, take a look at the following resources:
+### *Open your browser and visit:
+    http://localhost:3000
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+### Fork the repository.
+  #### Create a new branch:
+        git checkout -b feature/your-feature-name
+  #### Make your changes and commit them:
+        git commit -m 'Add some feature'
+  #### Push to the branch
+        git push origin feature/your-feature-name
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+## Create a Pull Request.
+You can also create an issue if you find a bug or have a suggestion.
 
-## Deploy on Vercel
+## 📞 Contact
+If you have any questions or need further assistance, feel free to open an issue or contact me at ubaidahmed2040@gmail.com.
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+___
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+# Happy epic coders 🎉
+
+___
+
--- src/compnents/ProfileCard.jsx
+++ src/compnents/ProfileCard.jsx
@@ -14,9 +14,9 @@ const ProfileCard = () => {
       </div>
 
       {/* Avatar and Content */}
-      <div className="p-4 text-center">
+      <div className="p-4 text-center flex flex-col items-center ">
         {/* Avatar */}
-        <div className="relative -mt-12 left-10   ">
+        <div className="relative bottom-16">
           <img
             className="w-32 h-32 md:w-40 md:h-40 object-cover  rounded-full border-4 border-white shadow-md"
             src="/Images/logo.jpg"
@@ -25,7 +25,7 @@ const ProfileCard = () => {
         </div>
 
         {/* Title and Subtitle */}
-        <div className="mt-16 mb-2 text-xl md:text-2xl text-light font-semibold text-shadow-custom">
+        <div className=" mb-2 text-xl md:text-2xl text-light font-semibold text-shadow-custom">
           Ubaid Ahmed
         </div>
         <div className="text-gray-600 text-base md:text-lg">
@@ -36,8 +36,8 @@ const ProfileCard = () => {
         <div className="mt-4">
           <ul className="flex justify-center space-x-4">
             {/* Facebook */}
-            <li className='bg-dark p-3 rounded-full'>
-              <a href="https://www.facebook.com/people/ubaidahmed04" target="_blank" rel="noopener noreferrer" className="text-light hover:text-blue-800">
+            <li className='bg-dark p-3 rounded-full hover:bg-black'>
+              <a href="https://www.facebook.com/people/ubaidahmed04" target="_blank" rel="noopener noreferrer" className="text-light hover:text-light-blue-400">
                 <span className="sr-only">Facebook</span>
                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 320 512">
                   <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
@@ -45,8 +45,8 @@ const ProfileCard = () => {
               </a>
             </li>
             {/* LinkedIn */}
-            <li className='bg-dark p-3 rounded-full'>
-              <a href="https://www.linkedin.com/in/sharjeel-hussain-2820b5293/" target="_blank" rel="noopener noreferrer" className="text-light hover:text-blue-900">
+            <li className='bg-dark p-3 hover:bg-black rounded-full hover:text-light-blue-700 '>
+              <a href="https://www.linkedin.com/in/sharjeel-hussain-2820b5293/" target="_blank" rel="noopener noreferrer" className="text-light hover:text-light-blue-700 ">
                 <span className="sr-only">LinkedIn</span>
                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 448 512">
                   <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path>
@@ -54,7 +54,7 @@ const ProfileCard = () => {
               </a>
             </li>
             {/* Instagram */}
-            <li className='bg-dark p-3 rounded-full'>
+            <li className='bg-dark p-3 rounded-full hover:bg-black'>
               <a href="https://www.instagram.com/sharjeelhussain877/" target="_blank" rel="noopener noreferrer" className="text-light hover:text-pink-700">
                 <span className="sr-only">Instagram</span>
                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 448 512">
